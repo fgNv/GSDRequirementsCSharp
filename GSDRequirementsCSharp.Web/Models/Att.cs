cb7a61825d1d08d453ad6c0becd64f02e79758de
fgUnderlost <felipe.underlost@gmail.com>
2016-03-28 10:38:13 -0300

    :white_check_mark: adding createUserCommand validation tests

---
+++ GSDRequirementsCSharp.Web/Models/Att.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GSDRequirementsCSharp.Web.Models
+{
+    public class Att
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        public Att(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public static Att NgModel(string value)
+        {
+            return new Att("data-ng-model", value);
+        }
+    }
+}
