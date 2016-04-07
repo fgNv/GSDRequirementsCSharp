dfb90dde736b9fdc29d03f5ba5fe1001111c5fcf
fgUnderlost <felipe.underlost@gmail.com>
2016-04-06 23:52:34 -0300

    :rocket: changing deployment order

--- deploy.cmd
+++ deploy.cmd
@@ -99,23 +99,6 @@ goto :EOF
 :: Deployment
 :: ----------
 
-:Deployment
-echo Handling .NET Web Application deployment.
-
-:: 1. Restore NuGet packages
-IF /I "GSDRequirementsCSharp.sln" NEQ "" (
-  call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\GSDRequirementsCSharp.sln"
-  IF !ERRORLEVEL! NEQ 0 goto error
-)
-
-:: 2. Build to the temporary path
-IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\GSDRequirementsCSharp.Web\GSDRequirementsCSharp.Web.csproj" /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;UseSharedCompilation=false /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
-) ELSE (
-  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\GSDRequirementsCSharp.Web\GSDRequirementsCSharp.Web.csproj" /nologo /verbosity:m /t:Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;UseSharedCompilation=false /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
-)
-
-IF !ERRORLEVEL! NEQ 0 goto error
 
 echo Handling node.js deployment.
 
@@ -147,6 +130,24 @@ IF EXIST "gruntfile.js" (
 )
 
 popd
+
+:Deployment
+echo Handling .NET Web Application deployment.
+
+:: 1. Restore NuGet packages
+IF /I "GSDRequirementsCSharp.sln" NEQ "" (
+  call :ExecuteCmd nuget restore "%DEPLOYMENT_SOURCE%\GSDRequirementsCSharp.sln"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
+
+:: 2. Build to the temporary path
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\GSDRequirementsCSharp.Web\GSDRequirementsCSharp.Web.csproj" /nologo /verbosity:m /t:Build /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="%DEPLOYMENT_TEMP%";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;UseSharedCompilation=false /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+) ELSE (
+  call :ExecuteCmd "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\GSDRequirementsCSharp.Web\GSDRequirementsCSharp.Web.csproj" /nologo /verbosity:m /t:Build /p:AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;UseSharedCompilation=false /p:SolutionDir="%DEPLOYMENT_SOURCE%\.\\" %SCM_BUILD_ARGS%
+)
+
+IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 3. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
