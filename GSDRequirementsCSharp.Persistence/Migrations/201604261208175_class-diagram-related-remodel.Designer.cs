5c2d5a4f813a9a6f9eecd2127ca2b7db0209ca07
fgUnderlost <felipe.underlost@gmail.com>
2016-04-26 09:11:44 -0300

    class diagram remodel migrations

--- GSDRequirementsCSharp.Persistence/Migrations/201604261208175_class-diagram-related-remodel.Designer.cs
+++ GSDRequirementsCSharp.Persistence/Migrations/201604261208175_class-diagram-related-remodel.Designer.cs
@@ -7,13 +7,13 @@ namespace GSDRequirementsCSharp.Persistence.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class classdiagram : IMigrationMetadata
+    public sealed partial class classdiagramrelatedremodel : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(classdiagram));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(classdiagramrelatedremodel));
         
         string IMigrationMetadata.Id
         {
-            get { return "201604261149252_class-diagram"; }
+            get { return "201604261208175_class-diagram-related-remodel"; }
         }
         
         string IMigrationMetadata.Source
