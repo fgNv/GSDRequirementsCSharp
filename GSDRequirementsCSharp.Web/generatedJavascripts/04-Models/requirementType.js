47d888ffb5361737c50998d0277614d65a9f937d
fgUnderlost <felipe.underlost@gmail.com>
2016-05-04 11:01:24 -0300

    readjusting typescript compilation

--- GSDRequirementsCSharp.Web/generatedJavascripts/04-Models/requirementType.js
+++
@@ -1,9 +0,0 @@
-var Models;
-(function (Models) {
-    (function (RequirementType) {
-        RequirementType[RequirementType["functional"] = 50] = "functional";
-        RequirementType[RequirementType["nonFunction"] = 800] = "nonFunction";
-    })(Models.RequirementType || (Models.RequirementType = {}));
-    var RequirementType = Models.RequirementType;
-})(Models || (Models = {}));
-//# sourceMappingURL=requirementType.js.map
